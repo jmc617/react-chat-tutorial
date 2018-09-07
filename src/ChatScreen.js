3c6a2c42435a3bcbeb5db2013aedb062eb9316d7
jmc617 <jmc617@gmail.com>
2018-09-07 19:12:43 -0400

    message list

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import Chatkit from '@pusher/chatkit'
+import MessageList from './components/MessageList'
 
 class ChatScreen extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            currentUser: {}
+            currentUser: {},
+            currentRoom: {},
+            messages: []
         }
     }
 
@@ -26,11 +29,44 @@ class ChatScreen extends Component {
             .catch(error => console.error('error', error))
     }
     render() {
+        const styles = {
+            container: {
+                height: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+            },
+            chatContainer: {
+                display: 'flex',
+                flex: 1,
+            },
+            whosOnlineListContainer: {
+                width: '300px',
+                flex: 'none',
+                padding: 20,
+                backgroundColor: '#2c303b',
+                color: 'white',
+            },
+            chatListContainer: {
+                padding: 20,
+                width: '85%',
+                display: 'flex',
+                flexDirection: 'column',
+            },
+        }
+
         return (
-            <div>
-                <h1>Chat</h1>
+            <div style={styles.container}>
+                <div style={styles.chatContainer}>
+                    <aside style={styles.whosOnlineListContainer}>
+                        <h2>Who's online Placeholder</h2>
+                    </aside>
+                    <section style={styles.chatListContainer}>
+                        <h2>Chat Placeholder</h2>
+                    </section>
+                </div>
             </div>
         )
+       
     }
 }
 
